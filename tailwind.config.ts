6487710b00fedb6339edf8f5d05dd05e4d2ba935
Bartosz <bdrozdek@gmail.com>
2024-04-19 11:09:50 +0200

    style: add custon colors and breakpoints

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,38 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-    },
-  },
-  plugins: [],
-};
-export default config;
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		screens: {
+			sm: '376px',
+			md: '768px',
+			lg: '976px',
+			xl: '1440px',
+		},
+		extend: {
+			colors: {
+				dark: {
+					cyan: '#0E8784',
+					grey: '#333D4B',
+				},
+				pale: {
+					orange: '#FDD6BA',
+					gray: '#E2DEDB',
+				},
+				light: {
+					cream: '#FEFCF7',
+					cyan: '#66D2CF',
+					yellow: '#F4F1EB',
+					salmon: 'FDD6BA',
+				},
+				grey: '#83888F',
+			},
+		},
+	},
+	plugins: [],
+}
+export default config
